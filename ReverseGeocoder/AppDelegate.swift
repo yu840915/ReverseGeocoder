0e7d7c23ce69056dbdaa8bd9a88453dd836215b9
LihsuanYu <yu840915@gmail.com>
2016-05-15 22:21:59 +0800

    Initial Commit

---
+++ ReverseGeocoder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReverseGeocoder
+//
+//  Created by small on 5/15/16.
+//  Copyright © 2016 small. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ReverseGeocoder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReverseGeocoder
+//
+//  Created by small on 5/15/16.
+//  Copyright © 2016 small. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
